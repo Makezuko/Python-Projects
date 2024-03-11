95c522081765b88cf06e5b40fb45deff7c10db8d
Matias Amma <matiasamma@gmail.com>
2024-03-11 02:13:54 -0300

    Ex21.py

---
+++ Exercísios André/Ex21.py
@@ -0,0 +1,19 @@
+def programa():
+    try:
+        lado = float(input('Insira o valor do lado do um retângulo em cm: '))
+        if lado <= 0:
+            print('Erro! O valor do lado precisa ser positivo.')
+            lado = float(input('Insira o valor do lado do retângulo em cm: '))
+        if lado > 0:
+            base = float(input('Insira o valor da base do retângulo em cm: '))
+            if base <= 0: 
+                print('Erro! O valor do lado precisa ser positivo.')
+                base = float(input('Insira o valor da base do retângulo em cm: '))
+            if base > 0:
+                area = base * lado
+                print('O valor da área de um retângulo de lado {:.2f}cm e base {:.2f}cm é de {:.2f}cm'.format(lado, base, area))
+    except ValueError:
+        print('Erro! Você precisa inserir um valor numérico.')
+        programa()
+print('Bem-vindo ao programa de área de um retângulo')
+programa()
