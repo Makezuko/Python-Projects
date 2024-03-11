1e71e7ebd42082f68c6eb9dcecaeebbe2701fd9c
Matias Amma <matiasamma@gmail.com>
2024-03-11 02:22:03 -0300

    Ex22.py

---
+++ Exercísios André/Ex.22.py
@@ -0,0 +1,19 @@
+def programa():
+    try:
+        diagonalMaior = float(input('Insira o valor da maior diagonal do paralelogramo em cm: '))
+        if diagonalMaior <= 0:
+            print('Erro! o valor inserido precisa ser positivo.')
+            diagonalMaior = float(input('Insira o valor da maior diaginal do paralelogramo em cm: '))
+        if diagonalMaior > 0:
+            diagonalMenor = float(input('Insira o valor da menor diagonal do paralelogramo em cm: '))
+        if diagonalMenor <= 0:
+            print('Erro! o valor inserido precisa ser positivo.')
+            diagonalMenor = float(input('Insira o valor da menor diagonal do paralelogramo em cm: '))
+        if diagonalMenor > 0:
+            area = (diagonalMenor * diagonalMaior)/2
+            print('O valor da área de um paralelogramo de diametros {:.2f}cm e {:.2f}cm é de {:.2f}cm quadrados'.format(diagonalMenor, diagonalMaior, area))
+    except ValueError:
+        print('Erro! Você precisa inserir um valor numérico.')
+        programa()
+print('Bem-vindo ao programa de área de um paralelogramo')
+programa()
