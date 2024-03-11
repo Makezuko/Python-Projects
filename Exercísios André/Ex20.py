668e4ba852b0215149b8dba3997694b48eaae4bc
Matias Amma <matiasamma@gmail.com>
2024-03-11 02:07:45 -0300

    Ex 20.py

---
+++ Exercísios André/Ex20.py
@@ -0,0 +1,14 @@
+def programa():
+    try:
+        lado = float(input('Insira o valor do lado do quadrado em cm: '))
+        if lado <= 0:
+            print('Erro! O valor do lado precisa ser positivo.')
+            lado = float(input('Insira o valor do lado do quadrado em cm: '))
+        if lado > 0:
+            area = lado**2
+            print('O valor da área de um quadrado de lado {:.2f}cm é de {:.2f}cm quadrados'.format(lado, area))
+    except ValueError:
+        print('Erro! Você precisa inserir um valor numérico.')
+        programa()
+print('Bem-vindo ao programa de área de um quadrado')
+programa()
