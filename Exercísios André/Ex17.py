194b7aedf9a41c90b254784253b95231dbb502f0
Matias Amma <matiasamma@gmail.com>
2024-03-11 01:22:31 -0300

    Ex17

---
+++ Exercísios André/Ex17.py
@@ -0,0 +1,18 @@
+def programa():
+    try:
+        lados = int(input('Insira a quantidade de lados que o polígono regular possui: '))
+        if lados <= 0:
+            print('Erro! Você precisa inserir um valor positivo.')
+            lados = int(input('Insira a quantidade de lados que o polígono regular possui: '))
+        if lados > 0:
+            valor_lados = float(input('Insira o valor de um dos lados do polígono em cm: '))
+            while valor_lados <= 0:
+                print('Erro! Você precisa inserir um valor positivo')
+                valor_lados = float(input('Insira o valor de um dos lados do polígono em cm: '))
+            perimetro = lados * valor_lados
+            print('O valor do perímetro de um polígono regular de {} lados de {:.2f}cm é de {:.2f}cm'.format(lados, valor_lados, perimetro))
+    except ValueError:
+        print('Erro! Você precisa inserir um valor numérico')
+        programa()
+print('Bem-vindo/a à calculadora de perímetros de polígonos regulares.')
+programa()
