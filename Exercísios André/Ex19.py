fbfa40047f91be8aa3ab445c82812421055c0c7c
Matias Amma <matiasamma@gmail.com>
2024-03-11 01:54:20 -0300

    Ex19

---
+++ Exercísios André/Ex19.py
@@ -0,0 +1,19 @@
+def programa():
+    try:
+        base = float(input('Insira o valor da base do triângulo em cm: '))
+        if base <= 0:
+            print('Erro! Você precisa inserir um valor positivo.')
+            base = float(input('Insira o valor da base do triângulo em cm: '))
+        if base > 0:
+            altura = float(input('Insiria o valor da base do triângulo em cm: '))
+            if altura <= 0:
+                print('Erro! Você precisa inserir um valor positivo.')
+                altura = float(input('Insiria o valor da base do triângulo em cm: '))
+            if altura > 0:
+                area = (base * altura)/2
+                print('O valor da área de um triângulo com base {:.2f}cm e altura {:.2f}cm é de {:.2f}cm quadrados.'.format(base, altura, area))
+    except ValueError:
+        print('Erro! Você precisa inserir um valor numérico.')
+        programa()
+print('Bem-vindo ao programa de área de um triângulo')
+programa()
