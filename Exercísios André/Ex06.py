90955eab202217877fd1f0175329ede464356522
Matias Amma <matiasamma@gmail.com>
2024-03-11 03:51:23 -0300

    Corrigindo formula do Ex6

--- Exercísios André/Ex06.py
+++ Exercísios André/Ex06.py
@@ -2,7 +2,7 @@ try:
     print('='*75)
     R = float(input('Insira a temperatura em Rankine que deseja converter a Celsius: '))
     print('='*75)
-    C = (R*1/8) - 419.67
+    C = (R - 491.67) * (5/9)
     print('{:.2f}°R são aproximadamente {:.2f}°C'.format(R, C))
 except ValueError:      
     print('='*75)
