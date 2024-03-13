6d06c657ee4be4c55ee2d78b56fed7097275f347
Matias Amma <matiasamma@gmail.com>
2024-03-13 03:16:02 -0300

    Falta consertar _igualdade

--- Exercísios André/Ex28V0.2.0.py
+++ Exercísios André/Ex28V0.2.0.py
@@ -1,71 +1,38 @@
-class Temperatura:
-    def input_usuario1():
-        try:
-            print('Insira qual temperatura você deseja converter.')
-            matriz_a = int(input('Celsius (1), Fahrenheit (2), Kelvin (3) ou Rankine (4): '))
-            print('Insira a temperatura a qual você deseja converter')
-            matriz_b = int(input('Celsius (1), Fahrenheit (2), Kelvin (3) ou Rankine (4): '))
-            while 0 >= matriz_a or matriz_a > 4 or 0 >= matriz_b or matriz_b > 4:
-                print('Erro! Você precisa inserir um dos valores pedidos')
-                print('Insira qual temperatura você deseja converter.')
-                matriz_a = int(input('Celsius (1), Fahrenheit (2), Kelvin (3) ou Rankine (4): '))
-                print('Insira a temperatura a qual você deseja converter')
-                matriz_b = int(input('Celsius (1), Fahrenheit (2), Kelvin (3) ou Rankine (4): '))
-        except ValueError:
-            print('Erro! Você precisa inserir valores numéricos')
-            Temperatura.input_usuario1()
+try:
+    print('Insira que temperatura você deseja converter.')
+    matriz_a = int(input('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3): '))
+    print('Insira a temperatura a qual você deseja converter')
+    matriz_b = int(input('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3): '))
+    while 0 > matriz_a or matriz_a > 3 or 0 > matriz_b or matriz_b > 3:
+        print('Erro! Você precisa inserir um dos valores pedidos')
+        print('Insira que temperatura você deseja converter.')
+        matriz_a = int(input('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3): '))
+        print('Insira a temperatura a qual você deseja converter')
+        matriz_b = int(input('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3): '))
+    x = float(input('Insira o valor da temperatura a ser convertida: '))
+except ValueError:
+    print('Erro! Você precisa inserir valores numéricos')
 
-    def input_usuario2():
-        x = float(input('Insira o valor da temperatura a ser convertida, usando o ponto como virgula caso necessário: '))
+if matriz_a == 0:
+    escala_a = 'C'
 
-    def _identidade(X):
-        print('')
-    
-    def _celsiusParaFarenheit(x):
-        return x * 9/5 + 32
-    
-    def _celsiusParaKelvin(x):
-        return x + 273.15
+if matriz_a == 1:
+    escala_a = 'F'
 
-    def _celsiusParaRankine(x):
-        return (x + 273.15)*(9/5)
-    
-    def _fahrenheitParaCelsius(x):
-        return (x - 32) * 5/9
-    
-    def _fahrenheitParaKelvin(x):
-        return (x + 459.67) * 5/9
-    
-    def _fahrenheitParaRankine(x):
-        return (x + 459.67)
-    
-    def _kelvinParaCelsius(x):
-        return (x - 273.15)
-    
-    def _kelvinParaFahrenheit(x):
-        return (x - 273.15)  (5/9)+ 32
-    
-    def _kelvinParaRankine(x):
-        return x * (9/5)
-    
-    def _rankineParaCelsius(x):
-        return (x - 491.67) * (5/9)
-    
-    def _rankineParaFahrenheit(x):
-        return x - 459.67
-    
-    def _rankineParaKelvin(x):
-        return x * (9/5)
-    
-    conversão = [
-        # Celsius             Farenheigt               Kelvin              Rankine
-        [_identidade, _celsiusParaFarenheit, _celsiusParaKelvin, _celsiusParaRankine],            # Celsius
-        [_fahrenheitParaCelsius, _identidade, _fahrenheitParaKelvin, _fahrenheitParaRankine],     # Farenheit
-        [_kelvinParaCelsius, _kelvinParaFahrenheit, _identidade, _kelvinParaRankine],             # Kelvin
-        [_rankineParaCelsius, _rankineParaFahrenheit, _rankineParaKelvin, _identidade]            # Rankine
-    ]
+if matriz_a == 2:
+    escala_a = 'K' 
 
-Temperatura.input_usuario1()
+if matriz_a == 3:
+    escala_a = 'R' 
 
-# obj = Temperatura()
-# print(f"{z} celsius = {obj.conversão[Temperatura.CELSIUS][Temperatura.CELSIUS](z)} celsius")
+if matriz_b == 0:
+    escala_b = 'C' 
+
+if matriz_b == 1:
+    escala_b = 'F' 
+
+if matriz_b == 2:
+    escala_b = 'K' 
+
+if matriz_b == 3:
+    escala_b = 'R'     
