132d3613fa6ac8e6e2420862004b411b26d9e3a6
Matias Amma <matiasamma@gmail.com>
2024-03-14 01:38:55 -0300

    Usando lista para verificar se as matrizes estão dentro dos parametros esperados

--- Exercísios André/Ex28V0.2.0.py
+++ Exercísios André/Ex28V0.2.0.py
@@ -1,8 +1,10 @@
+temperatura = [0, 1, 2, 3]
+
 try:
     print('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3)')
     matriz_a = int(input('Insira que temperatura você deseja converter: '))
     matriz_b = int(input('Insira a temperatura a qual você deseja converter: '))
-    while 0 > matriz_a or matriz_a > 3 or 0 > matriz_b or matriz_b > 3:
+    while matriz_a not in temperatura or matriz_b not in temperatura:
         print('Erro! Você precisa inserir um dos valores pedidos')
         print('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3)')
         matriz_a = int(input('Insira que temperatura você deseja converter: '))
@@ -33,50 +35,50 @@ if matriz_b == 2:
     escala_b = 'K' 
 
 if matriz_b == 3:
-    escala_b = 'R'   
+    escala_b = 'R'
 
-def _celsiusParaFahrenheit():
+def _celsiusParaFahrenheit(x):
     return (x * 9/5) + 32
 
-def _celsiusParaKelvin():
+def _celsiusParaKelvin(x):
     return x + 273.15
 
-def _celsiusParaRankine():
+def _celsiusParaRankine(x):
     return (x + 273.15)*(9/5)
     
-def _fahrenheitParaCelsius():
+def _fahrenheitParaCelsius(x):
     return (x - 32) * 5/9
     
-def _fahrenheitParaKelvin():
+def _fahrenheitParaKelvin(x):
     return (x + 459.67) * 5/9
     
-def _fahrenheitParaRankine():
+def _fahrenheitParaRankine(x):
     return (x + 459.67)
     
-def _kelvinParaCelsius():
+def _kelvinParaCelsius(x):
     return (x - 273.15)
     
-def _kelvinParaFahrenheit():
+def _kelvinParaFahrenheit(x):
     return (x - 273.15) * (5/9) + 32
     
-def _kelvinParaRankine():
+def _kelvinParaRankine(x):
     return x * (9/5)
     
-def _rankineParaCelsius():
+def _rankineParaCelsius(x):
     return (x - 491.67) * (5/9)
     
-def _rankineParaFahrenheit():
+def _rankineParaFahrenheit(x):
     return x - 459.67
     
-def _rankineParaKelvin():
+def _rankineParaKelvin(x):
     return x / (9/5)
 
 conversão = [
         # Celsius             Farenheigt               Kelvin              Rankine
-        ['As Escalas são iguais', _celsiusParaFahrenheit(), _celsiusParaKelvin(), _celsiusParaRankine()],            # Celsius
-        [_fahrenheitParaCelsius(), 'As Escalas são iguais', _fahrenheitParaKelvin(), _fahrenheitParaRankine()],     # Farenheit
-        [_kelvinParaCelsius(), _kelvinParaFahrenheit(), 'As Escalas são iguais', _kelvinParaRankine()],             # Kelvin
-        [_rankineParaCelsius(), _rankineParaFahrenheit(), _rankineParaKelvin(), 'As Escalas são iguais']            # Rankine
+        ['As Escalas são iguais', _celsiusParaFahrenheit(x), _celsiusParaKelvin(x), _celsiusParaRankine(x)],            # Celsius
+        [_fahrenheitParaCelsius(x), 'As Escalas são iguais', _fahrenheitParaKelvin(x), _fahrenheitParaRankine(x)],     # Farenheit
+        [_kelvinParaCelsius(x), _kelvinParaFahrenheit(x), 'As Escalas são iguais', _kelvinParaRankine(x)],             # Kelvin
+        [_rankineParaCelsius(x), _rankineParaFahrenheit(x), _rankineParaKelvin(x), 'As Escalas são iguais']            # Rankine
     ]
 try:
     print('{:.2f}°{} é aproximadamente {:.2f}°{}'.format(x, escala_a, conversão[matriz_a][matriz_b], escala_b))
