391feddc111b349e53a42ae280661824ba0c8f85
MATIAS AMMA <matias.a@puccampinas.edu.br>
2024-03-19 19:13:14 -0300

    Alternando nomes do Ex.28

--- Exercísios André/Ex28.py
+++ Exercísios André/Ex28.py
@@ -1,92 +1,70 @@
-from Ex01 import programa1
-from Ex02 import programa2
-from Ex03 import programa3
-from Ex04 import programa4
-from Ex05 import programa5
-from Ex06 import programa6
-from Ex07 import programa7
-from Ex08 import programa8
-from Ex09 import programa9
-from Ex10 import programa10
-from Ex11 import programa11
-from Ex12 import programa12
+input_possivel = [0, 1, 2, 3]
 
-def programa():
-    try:
-        print('='*75)
-        print("Bem-vindo ao programa que calcula diferentes tipos de temperaturas.")
-        print("Qual a temperatura que você deseja converter?")
-        print('='*75)
-        temperatura_a_converter = int(input("Celsius (1), Fahrenheit (2), Kelvin (3), Rankine (4): "))
-        print('='*75)
-        while temperatura_a_converter <= 0 or temperatura_a_converter > 4:
-            print("Erro! Por favor insira um dos valores pedidos.")
-            print('='*75)
-            print("Qual a temperatura que você deseja converter?")
-            temperatura_a_converter = int(input("Celsius (1), Fahrenheit (2), Kelvin (3), Rankine (4): "))
-        print("A qual temperatura que você deseja converter?")
-        temperatura_a_ser_convertida = int(input("Celsius (1), Fahrenheit (2), Kelvin (3), Rankine (4): "))
-        while temperatura_a_ser_convertida <= 0 or temperatura_a_ser_convertida > 4:
-            print("Erro! Por favor insira um dos valores pedidos.")
-            print('='*75)
-            print("A Qual temperatura que você deseja converter?")
-            print('='*75)
-            temperatura_a_ser_convertida = int(input("Celsius (1), Fahrenheit (2), Kelvin (3), Rankine (4): "))
-        if temperatura_a_converter == temperatura_a_ser_convertida:
-            print("Erro! Para o programa funcionar escolha 2 escalas de temperatura diferentes.")
-            programa()
-        if temperatura_a_converter == 1 and temperatura_a_ser_convertida == 2:
-            programa1()
-        if temperatura_a_converter == 1 and temperatura_a_ser_convertida == 3:
-            programa3()
-        if temperatura_a_converter == 1 and temperatura_a_ser_convertida == 4:
-            programa5()
-        if temperatura_a_converter == 2 and temperatura_a_ser_convertida == 1:
-            programa2()
-        if temperatura_a_converter == 2 and temperatura_a_ser_convertida == 3:
-            programa7()
-        if temperatura_a_converter == 2 and temperatura_a_ser_convertida == 4:
-            programa9()
-        if temperatura_a_converter == 3 and temperatura_a_ser_convertida == 1:
-            programa4()
-        if temperatura_a_converter == 3 and temperatura_a_ser_convertida == 2:
-            programa8()
-        if temperatura_a_converter == 3 and temperatura_a_ser_convertida == 4:
-            programa11()
-        if temperatura_a_converter == 4 and temperatura_a_ser_convertida == 1:
-            programa6()
-        if temperatura_a_converter == 4 and temperatura_a_ser_convertida == 2:
-            programa10()
-        if temperatura_a_converter == 4 and temperatura_a_ser_convertida == 3:
-            programa12()
-    except ValueError:
-        print("Erro! Os valores inseridos devem ser numéricos")
-        programa()
-#programa()
+escalas = {
+    0: '°C',
+    1: '°F',
+    2: '°K',
+    3: '°R'
+}
 
+try:
+    print('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3)')
+    matriz_a = int(input('Insira que temperatura você deseja converter: '))
+    matriz_b = int(input('Insira a temperatura a qual você deseja converter: '))
+    while matriz_a not in input_possivel or matriz_b not in input_possivel:
+        print('Erro! Você precisa inserir um dos valores pedidos')
+        print('Celsius (0), Fahrenheit (1), Kelvin (2) ou Rankine (3)')
+        matriz_a = int(input('Insira que temperatura você deseja converter: '))
+        matriz_b = int(input('Insira a temperatura a qual você deseja converter: '))
+    x = float(input('Insira o valor da temperatura a ser convertida: '))
+except ValueError:
+    print('Erro! Você precisa inserir valores numéricos')
 
-class Temperatura:
+def _celsiusParaFahrenheit(x):
+    return (x * 9/5) + 32
 
-    CELSIUS = 1
-    FARENHEIT = 2
-    KELVIN = 3
-    RANKINE = 4
+def _celsiusParaKelvin(x):
+    return x + 273.15
 
-    def _identidade(X):
-        return X
+def _celsiusParaRankine(x):
+    return (x + 273.15)*(9/5)
     
-    def _celsiusParaFarenheit(x):
-        return x*9/5 + 32
+def _fahrenheitParaCelsius(x):
+    return (x - 32) * 5/9
+    
+def _fahrenheitParaKelvin(x):
+    return (x + 459.67) * 5/9
+    
+def _fahrenheitParaRankine(x):
+    return (x + 459.67)
+    
+def _kelvinParaCelsius(x):
+    return (x - 273.15)
+    
+def _kelvinParaFahrenheit(x):
+    return (x - 273.15) * (5/9) + 32
+    
+def _kelvinParaRankine(x):
+    return x * (9/5)
+    
+def _rankineParaCelsius(x):
+    return (x - 491.67) * (5/9)
+    
+def _rankineParaFahrenheit(x):
+    return x - 459.67
+    
+def _rankineParaKelvin(x):
+    return x / (9/5)
 
-    conversão = [
-        # Celsius   Farenheigt  Kelvin  Rankine
-        [_identidade, _celsiusParaFarenheit, programa3, programa5],   # Celsius
-        [programa2, _identidade, programa7, programa9],              # Farenheit
-        [programa4, programa8, _identidade, programa11],             # Kelvin
-        [programa6, programa10, programa12, _identidade]             # Rankine
+conversão = [
+        # Celsius             Farenheigt               Kelvin              Rankine
+        ['As Escalas são iguais', _celsiusParaFahrenheit(x), _celsiusParaKelvin(x), _celsiusParaRankine(x)],            # Celsius
+        [_fahrenheitParaCelsius(x), 'As Escalas são iguais', _fahrenheitParaKelvin(x), _fahrenheitParaRankine(x)],     # Farenheit
+        [_kelvinParaCelsius(x), _kelvinParaFahrenheit(x), 'As Escalas são iguais', _kelvinParaRankine(x)],             # Kelvin
+        [_rankineParaCelsius(x), _rankineParaFahrenheit(x), _rankineParaKelvin(x), 'As Escalas são iguais']            # Rankine
     ]
-
-z = 100
-
-obj = Temperatura()
-print(f"{z} celsius = {obj.conversão[Temperatura.CELSIUS][Temperatura.CELSIUS](z)} celsius")
+try:
+    print('{:.2f}{} é aproximadamente {:.2f}{}'.format(x, escalas[matriz_a], conversão[matriz_a][matriz_b], escalas[matriz_b]))
+except ValueError:
+    print(conversão[matriz_a][matriz_b])
+    print('{:.2f}{} é igual a {:.2f}{}'.format(x, escalas[matriz_a], x, escalas[matriz_b]))
